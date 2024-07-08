6ab09f8b94fd4a979373b47cf17ca3078f1160d2
Ezgi Senyurt-Ertürk <156966522+ezgsnyrt@users.noreply.github.com>
2024-07-08 12:15:31 +0200

    add keyboard action event listener and use it for showing correct guesses about letters on the screen

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
 // kelime listesi +
 // kelimedeki harf sayısı kadar çizgi +
-// tahmin edilen harf kelimede varsa, o harfi yerinde gösterme
+// tahmin edilen harf kelimede varsa, o harfi yerinde gösterme +
 // yanlış yapılan her tahmin için sırasıyla kafa, gövde, bacaklar ve kolları gösterme
 // tahmin edilen harfleri klavyede disabled yapma
-// sonucu gösterme (kelime bilindiyse, result text congratulations yazısı ve new game butonu gösterme)
+// sonucu gösterme (kelime bilindiyse, result text yazısı Congrats! You succesfully found the word ve new game butonu gösterme)
 //                 (kelime bilinemediyse, result text don't give up yazısı ve try again butonu)
 
+// STEP 1
 let wordArr = ["Watermelon", "Elephant", "Hippopotamus", "Cascade", "Pseudonym",  "Kaleidoscope", "Quantum", "Serendipity", "Umbrella", "Ephemeral", "Photosynthesis",
-"Algorithm", "Nostalgia", "Perpendicular", "Giraffe", "Nebula"];
+"Algorithm", "Nostalgia", "Perpendicular", "Giraffe", "Nebula"]; // Create a word array
 
-const randomWord = () => {
+const randomWord = () => {  // Select a random word from wordArr
   return(wordArr[Math.floor(Math.random() * wordArr.length)]);
 }
-console.log(randomWord());
+const selectedWord = randomWord().toLowerCase(); // Assign the randomWord as selectedWord
+console.log(selectedWord);
 
-//assign the random word as input word
-const inputWord = randomWord();
-for (let i = 0; i < inputWord.length; i++) {
-
-  //create span element for each letter and append it to user-input-section
-  let spanElement = document.createElement("span");
+// STEP 2
+for (let i = 0; i < selectedWord.length; i++) {
+  let spanElement = document.createElement("span"); // Create span element for each letter and append it to user-input-section
   spanElement.innerHTML = "_";
   document.getElementById("user-input-section").appendChild(spanElement);
 }
 
-//
-function keyboardAction () {
+// STEP 3
+let correctLetters = []; // Define a variable for correct guess
+let wrongGuessCount = 0; // Define a variable for wrong guess
 
+function keyboardAction () { //events: keydown, keyup etc. //document.addEventListener(event, callback)
+  document.addEventListener("keydown", event => {
+    console.log(event.key); //Since the letter is written as a value for key property of the event, include it
+
+    if(selectedWord.includes(event.key)) { // Check if selectedWord includes event.key
+      [...selectedWord].forEach((letter, index) => { //Show correct letters in the word
+        if(letter === event.key) {
+          correctLetters.push(letter);
+          document.querySelector("#user-input-section").querySelectorAll("span")[index].innerText = letter; // Another option is to create const displayWord = document.querySelector("#user-input-section")
+        }
+      });
+    } else { //if the clicked letter does not exist in the word
+      wrongGuessCount++;
+      //hangman body part will be shown.
+    }
+  });
 }
+keyboardAction();
+
+
+
+
+
 
 
 
