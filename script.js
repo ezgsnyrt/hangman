118cc2df3629fe6d2c9d5c8f849958ae0dee4350
Ezgi Senyurt-Ertürk <156966522+ezgsnyrt@users.noreply.github.com>
2024-07-12 15:09:54 +0200

    changing the locations of individual functions because of scope related limitations, change manage letter processes function by updating wrong letters in html if the virtual keyboard buttons are used and adding body parts if the user makes wrong guesses about letters, synchronizing keyboard and virtual keyboard actions

--- script.js
+++ script.js
@@ -49,10 +49,11 @@ generateButton();
 
 // Handle button click processes
 function handleClick(event) {
-  const buttonClicked = event.target.nodeName === "BUTTON"; //Check if the event.target element is a <button> element. event.target represents the DOM element triggering the event. The nodeName property returns the type of the element in uppercase (e.g., "BUTTON").
-  if (buttonClicked) {
+  const virtualButtonClicked = event.target.nodeName === "BUTTON"; // Check if the event.target element is a <button> element. event.target represents the DOM element triggering the event. The nodeName property returns the type of the element in uppercase (e.g., "BUTTON").
+  // const reviseWrongLetters = updateWrongLetters();
+  if (virtualButtonClicked ) { // || reviseWrongLetters
     //Use the event target id (the element triggering the event) to find and return the actual DOM element
-    const buttonId = document.getElementById(event.target.id); //Use event.target.id within the event handler to get the id attribute of the element that triggered the event (e.g., a button clicked), and event.target.id retrieves the id of that element.
+    const buttonId = document.getElementById(event.target.id); // Use event.target.id within the event handler to get the id attribute of the element that triggered the event (e.g., a button clicked), and event.target.id retrieves the id of that element.
     buttonId.classList.add("selected");
     const letter = event.target.id;
     manageLetterProcesses(letter);
@@ -71,79 +72,49 @@ function manageLetterProcesses(letter) {
   } else {
     if (!wrongLettersArr.includes(letter)) {
       wrongLettersArr.push(letter);
-      // updateWrongLetters()
+      updateWrongLetters() // Update wrong letters in html if the virtual keyboard buttons are clicked
+      showNextBodyPart(); // Add body parts if the user makes wrong guesses about letters
     }
   }
 }
 
+// Keep the wrong guesses of the user in wrongLettersArr[]
+function addWrongLetter(letter) {
 
-// STEP 3
-let correctLetters = []; // Define a variable for correct guess
-let wrongGuessCount = 0; // Define a variable for the number of wrong guesses
-const wrongLettersArr = []; // Define a variable for wrong letters
-
-function keyboardAction () { //events: keydown, keyup etc. //document.addEventListener(event, callback)
-  document.addEventListener("keydown", event => {
-    if (event.keyCode >= 65 && event.keyCode <= 90) { // Only consider letters, not other characters
-      console.log("Valid key:", event.key); // Since the letter is written as a value for key property of the event, include it
-
-      // Check if selectedWord includes event.key
-      if(selectedWord.includes(event.key)) {
-        [...selectedWord].forEach((letter, index) => { //Show correct letters in the word
-          if(letter === event.key) {
-            correctLetters.push(letter);
-            document.querySelector("#user-input-section").querySelectorAll("span")[index].innerText = letter; // Another option is to create const displayWord = document.querySelector("#user-input-section")
-          }
-        });
-      } else { // Show wrong letter trials if the clicked letter does not exist in the word
-        wrongGuessCount++;
-
-        addWrongLetter(event.key.toLowerCase()); // Take and update the wrong letters/inputs by means of keydown event (use it as a parameter)
-
-        // Keep the wrong guesses of the user in wrongLettersArr[]
-        function addWrongLetter(letter) {
-
-          if (!wrongLettersArr.includes(letter)) { // If the letter is not existed in the array, add it to the array
-            wrongLettersArr.push(letter);
-            showNextBodyPart(); //Show the body parts
-          }
-          updateWrongLetters(); // Update the wrong letters
-          // manageLetterProcesses();
-        }
-
-        function updateWrongLetters() {
-          let wrongLettersHTML = '';
-
-          if (wrongLettersArr.length > 0) {
-            wrongLettersHTML += '<h4>Wrong Letter Trials</h4>'; // If there are wrong letters in the guess, add the heading element to html
-          }
-
-          for (let i = 0; i < wrongLettersArr.length; i++) { // Add each wrong letter to html
-            wrongLettersHTML += `<span> ${wrongLettersArr[i]} </span>`;
-          }
-
-          document.getElementById("wrong-guess").innerHTML = wrongLettersHTML; // Upadate the innter html
-        }
-
-        // Alternative function
-        // function updateWrongLetters () {
-        //   wrongLetters.innerHTML = `
-        //     ${wrongLettersArr.length > 0 ? '<h4>Wrong Letter Trials</h4>' : ''}
-        //     ${wrongLettersArr.map(letter => `<span> ${letter} </span>`)}
-        //   `;
-        // }
-        // updateWrongLetters();
-        // if (wrongGuessCount === 0) { }
-      }
-    } else {
-      console.log("Invalid key:", event.key);
-    }
-  });
+  if (!wrongLettersArr.includes(letter)) { // If the letter is not existed in the array, add it to the array
+    wrongLettersArr.push(letter);
+    showNextBodyPart(); // Show the body parts
+  }
+  updateWrongLetters(); // Update the wrong letters
+  // updateWrongLettersAlternative (); as an alternative function call
 }
-keyboardAction();
 
 
-//Show hangman body parts considering each wrong guess
+function updateWrongLetters() {
+  let wrongLettersHTML = '';
+
+  if (wrongLettersArr.length > 0) {
+    wrongLettersHTML += '<h4>Wrong Letter Trials</h4>'; // If there are wrong letters in the guess, add the heading element to html
+  }
+
+  for (let i = 0; i < wrongLettersArr.length; i++) { // Add each wrong letter to html
+    wrongLettersHTML += `<span> ${wrongLettersArr[i]} </span>`;
+  }
+
+  document.getElementById("wrong-guess").innerHTML = wrongLettersHTML; // Update the inner html
+}
+
+// Alternative function about updating wrong letters
+// function updateWrongLettersAlternative () {
+//   wrongLetters.innerHTML = `
+//     ${wrongLettersArr.length > 0 ? '<h4>Wrong Letter Trials</h4>' : ''}
+//     ${wrongLettersArr.map(letter => `<span> ${letter} </span>`)}
+//   `;
+// }
+// updateWrongLetters();
+// if (wrongGuessCount === 0) { }
+
+// Show hangman body parts considering each wrong guess
 function showNextBodyPart () {
   // Define and select hangman body parts
   const hangmanBodyParts = document.querySelectorAll('.hangman-body-part');
@@ -156,5 +127,50 @@ function showNextBodyPart () {
     }
   }
 }
+
+
+// STEP 3
+let correctLetters = []; // Define a variable for correct guess
+let wrongGuessCount = 0; // Define a variable for the number of wrong guesses
+const wrongLettersArr = []; // Define a variable for wrong letters
+
+function keyboardAction () { // events: keydown, keyup etc. //document.addEventListener(event, callback)
+  document.addEventListener("keydown", event => {
+    if (event.keyCode >= 65 && event.keyCode <= 90) { // Only consider letters, not other characters
+      console.log("Valid key:", event.key); // Since the letter is written as a value for key property of the event, include it
+
+
+      // Check if selectedWord includes event.key
+      if(selectedWord.includes(event.key)) {
+        [...selectedWord].forEach((letter, index) => { // Show correct letters in the word
+          if(letter === event.key) {
+            correctLetters.push(letter);
+            document.querySelector("#user-input-section").querySelectorAll("span")[index].innerText = letter; // Another option is to create const displayWord = document.querySelector("#user-input-section")
+          }
+        });
+      } else { // Show wrong letter trials if the clicked letter does not exist in the word
+        wrongGuessCount++;
+
+        // Call function addWrongLetter(letter)
+        addWrongLetter(event.key.toLowerCase()); // Take and update the wrong letters/inputs by means of keydown event (use it as a parameter)
+
+        // updateWrongLetters() has already been called in addWrongLetters function
+      }
+
+        // Keyboard'da basılan event key'i burada "selected" yapmalıyım
+        // Use getElementById to select element with id. E.g., if 'a' is the clicked letter, buttonElement becomes <button id="a">...</button>
+        const buttonElement = document.getElementById(event.key); // letter refers to letters clicked in keyboard or virtual keyboard.
+        // If buttonElement exists (buttonElement !== null olduğunda) and it doesn't have a 'selected' class already, add 'selected' class
+        if (buttonElement && !buttonElement.classList.contains('selected')) { // Control buttonElement
+        buttonElement.classList.add('selected');
+        }
+    } else {
+      console.log("Invalid key:", event.key);
+    }
+  });
+}
+keyboardAction();
+
+
 
 //TODO: Upper case letters are shown in the wrong letter trials part even if they are included in the word.
